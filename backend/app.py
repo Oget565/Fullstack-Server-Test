a98417c610e05aa7bdc8cb481399816e0ea49d8f
Oget565 <149737437+Oget565@users.noreply.github.com>
2025-10-08 11:54:25 -0400

    backend function complete

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,19 @@
 from flask import Flask,  jsonify
+import python_weather
+import asyncio
 
 app = Flask(__name__)
+
+async def get_weather():
+    async with python_weather.Client() as client:
+        weather = await client.get('New York')
+        return weather.temperature
+
+@app.route('/request_weather')
+def request_weather():
+    # Fetch weather data on each request
+    weather_temp = asyncio.run(get_weather())
+    return jsonify({'weather': weather_temp})
+
+if __name__ == '__main__':
+    app.run(debug=False, port=8085)
